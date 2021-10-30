b5172adde62fa8df6a26f5f290894777d30a6200
Stefan Siegfried <78775438+stefansiegfried@users.noreply.github.com>
2021-10-29 23:32:08 -0400

    Initial commit

---
+++ libsuraimuengine/include/libsuraimuengine/game.h
@@ -0,0 +1,31 @@
+#ifndef LIBSURAIMUENGINE_GAME_H_INCLUDED
+#define LIBSURAIMUENGINE_GAME_H_INCLUDED
+
+#include <SDL.h>
+#include <string>
+
+namespace suraimu
+{
+
+  class Game
+  {
+  public:
+    Game(const char *title = "Game Window", int width = 640, int height = 480, bool fullscreen = false);
+    ~Game();
+    int Init();
+    SDL_Renderer *GetRenderer();
+
+  protected:
+    const char *game_window_title_;
+    const int game_window_width_;
+    const int game_window_height_;
+    const bool game_window_fullscreen_;
+
+    SDL_Window *game_window_ = nullptr;
+    SDL_Renderer *game_renderer_ = nullptr;
+    bool is_initialized_ = false;
+  };
+
+} // namespace suraimu
+
+#endif // LIBSURAIMUENGINE_GAME_H_INCLUDED
